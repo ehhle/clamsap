f5babb995860c945f393f414948dba17f84f44ce
ehhle <markus@strehle-net.de>
2021-06-17 11:24:56 +0000

    add rpm build script

---
+++ scripts/rpm_build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd ..
+aclocal
+autoconf
+autoheader
+libtoolize
+automake --add-missing
+autoreconf --install --force
+./configure
+make pack
+cd ..
+mkdir -p ~/rpmbuild
+rpmbuild -tb clamsap-*.tar.gz
+cp ~/rpmbuild/RPMS/x86_64/clamsap-0.103.2-1.x86_64.rpm .
