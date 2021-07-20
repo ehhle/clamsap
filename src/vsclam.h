32e182b02b41b49d5d9a9dc82795437e8aed9d36
Markus Strehle <markus.strehle@sap.com>
2021-07-20 14:58:02 +0200

    patch 0.103.3

--- src/vsclam.h
+++ src/vsclam.h
@@ -52,7 +52,7 @@ extern "C"
 #ifdef CLAMSAP_VERSION
 #define VSA_ADAPTER_VERSION    CLAMSAP_VERSION
 #else
-#define VSA_ADAPTER_VERSION    "0.103.2.0"
+#define VSA_ADAPTER_VERSION    "0.103.3.0"
 #endif
 #define VSA_ADAPTER_MAJVER     0
 #define VSA_ADAPTER_MINVER     1015
